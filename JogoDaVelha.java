665438acd4a3dfe7762251144a0934e2c893ec23
Amanda Fantuzze <105981624+FriendlyCheesecake@users.noreply.github.com>
2022-10-02 13:25:49 +0900

    Jogo da Velha Versão 2 de Outubro
    Versão do programa Jogo da Velha com verificações de ganhador e posição ocupada.

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -3,32 +3,96 @@ import java.util.Scanner;
 
 //Tabuleiro e While de Jogadas, mas sem verificação
 public class JogoDaVelha {
+    static int turns = 0;
+    static int firstPlayerPoints = 0;
+    static int secondPlayersPoints = 0;
+    static String firstPlayerName;
+    static String secondPlayerName;
+    static boolean Winner = false;
+    static boolean Draw = false;
+    static Scanner input = new Scanner(System.in);
+    static char[][] tabuleiro = {
+            {'1', '|', '2', '|', '3'},
+            {'4', '|', '5', '|', '6'},
+            {'7', '|', '8', '|', '9'}};
     public static void main(String[] args) {
-        char[][] tabuleiro = {{'1','|','2','|','3'},{'-','+','-','+','-'},{'4','|','5','|','6'},{'-','+','-','+','-'},{'7','|','8','|','9'}};
 
+        startGame();
+        System.out.println("Novo jogo iniciado:");
+        System.out.println();
+        playGame();
+    }
+    public static void playGame() {
         imprimeTabuleiro(tabuleiro);
         Scanner jogada = new Scanner(System.in);
 
-        while(true) {
+        while (!Winner || !Draw) {
             //Jogador1 faz sua jogada
-            System.out.println("Jogador 1, escolha e digite o número de uma posição vazia:");
-            int posicaoJogador1 = jogada.nextInt();
+            System.out.println(firstPlayerName + ", escolha e digite o número de uma posição vazia:");
+            char posicaoJogador1 = jogada.next().charAt(0);
 
-            //Exibe jogada jogador1
-            marcaPosicao(tabuleiro, posicaoJogador1, "jogador 1");
+            posicaoValidaJogador1(posicaoJogador1);
 
+            String jogadorChar = "XXX";
+            turns++;
+
+            verificarGanhador(tabuleiro, jogadorChar, firstPlayerName);
+            if (Winner) {
+                firstPlayerPoints++;
+                getPlacar(firstPlayerName, firstPlayerPoints, secondPlayerName, secondPlayersPoints);
+                break;
+            }
+
+            if (Draw) break;
             //Jogador2 faz sua jogada
-            System.out.println("Jogador 2, escolha e digite o número de uma posição vazia:");
+            System.out.println(secondPlayerName + ", escolha e digite o número de uma posição vazia:");
+            char posicaoJogador2 = jogada.next().charAt(0);
 
-            int posicaoJogador2 = jogada.nextInt();
+            posicaoValidaJogador2(posicaoJogador2);
 
-            //Exibe jogada jogador2
-            marcaPosicao(tabuleiro, posicaoJogador2, "jogador 2");
+            jogadorChar = "OOO";
+            turns++;
 
+            verificarGanhador(tabuleiro, jogadorChar, secondPlayerName);
+            if (Winner) {
+                secondPlayersPoints++;
+                getPlacar(secondPlayerName, secondPlayersPoints, firstPlayerName, firstPlayerPoints);
+                break;
+            }
+            if (Draw) break;
+        }
+    }
+
+
+    public static void startGame() {
+        System.out.println("Bem vindo ao Jogo da Velha! Gostaria de iniciar uma partida?");
+        System.out.println("| 1 - SIM   | 2 - NÃO  |");
+
+        switch (input.nextInt()) {
+            case 1:
+                System.out.println("Insira os dados dos jogadores: ");
+                getPlayerInformation();
+                break;
+            case 2:
+                System.out.println("Ah, que pena! Até a próxima!");
+                break;
         }
 
     }
-    //Imprimir tabuleiro
+
+    public static void getPlayerInformation() {
+
+
+        System.out.println("Qual o nome do primeiro jogador? ");
+        firstPlayerName = input.next();
+        System.out.println(firstPlayerName + ", você será o X !");
+
+        System.out.println("Qual o nome do segundo jogador? ");
+        secondPlayerName = input.next();
+        System.out.println(secondPlayerName + ", você será o O !");
+
+    }
+
     public static void imprimeTabuleiro(char[][] tabuleiro) {
 
         for (int i = 0; i < tabuleiro.length; i++) {
@@ -38,44 +102,153 @@ public class JogoDaVelha {
             System.out.println();
         }
     }
-    public static void marcaPosicao(char[][] tabuleiro, int posicao, String jogador){
+
+    public static void marcaPosicao(char[][] tabuleiro, char posicao, String jogador) {
         //Impressão da jogada no tabuleiro
-        char marca = 'X';
-        if(jogador.equals("jogador 1")){
+        char marca = ' ';
+        if (jogador.equals("jogador 1")) {
             marca = 'X';
-        } else if(jogador.equals("jogador 2")){
+        } else if (jogador.equals("jogador 2")) {
             marca = 'O';
         }
 
-        switch(posicao) {
-            case 1:
+        switch (posicao) {
+            case '1':
                 tabuleiro[0][0] = marca;
                 break;
-            case 2:
+            case '2':
                 tabuleiro[0][2] = marca;
                 break;
-            case 3:
+            case '3':
                 tabuleiro[0][4] = marca;
                 break;
-            case 4:
+            case '4':
+                tabuleiro[1][0] = marca;
+                break;
+            case '5':
+                tabuleiro[1][2] = marca;
+                break;
+            case '6':
+                tabuleiro[1][4] = marca;
+                break;
+            case '7':
                 tabuleiro[2][0] = marca;
                 break;
-            case 5:
+            case '8':
                 tabuleiro[2][2] = marca;
                 break;
-            case 6:
+            case '9':
                 tabuleiro[2][4] = marca;
                 break;
-            case 7:
-                tabuleiro[4][0] = marca;
-                break;
-            case 8:
-                tabuleiro[4][2] = marca;
-                break;
-            case 9:
-                tabuleiro[4][4] = marca;
-                break;
         }
+
         imprimeTabuleiro(tabuleiro);
+    }
+
+    public static void posicaoValidaJogador1(char posicaoJogador1){
+        while ((posicaoJogador1 == '1' && (tabuleiro[0][0] == 'X' || tabuleiro[0][0] == 'O'))
+                || (posicaoJogador1 == '2' && (tabuleiro[0][2] == 'X' || tabuleiro[0][2] == 'O'))
+                || (posicaoJogador1 == '3' && (tabuleiro[0][4] == 'X' || tabuleiro[0][4] == 'O'))
+                || (posicaoJogador1 == '4' && (tabuleiro[1][0] == 'X' || tabuleiro[1][0] == 'O'))
+                || (posicaoJogador1 == '5' && (tabuleiro[1][2] == 'X' || tabuleiro[1][2] == 'O'))
+                || (posicaoJogador1 == '6' && (tabuleiro[1][4] == 'X' || tabuleiro[1][4] == 'O'))
+                || (posicaoJogador1 == '7' && (tabuleiro[2][0] == 'X' || tabuleiro[2][0] == 'O'))
+                || (posicaoJogador1 == '8' && (tabuleiro[2][2] == 'X' || tabuleiro[2][2] == 'O'))
+                || (posicaoJogador1 == '9' && (tabuleiro[2][4] == 'X' || tabuleiro[2][4] == 'O'))){
+            System.out.println("Jogada inválda, escolha outra posição: ");
+            Scanner jogada = new Scanner(System.in);
+            posicaoJogador1 = jogada.next().charAt(0);
+
+        } marcaPosicao(tabuleiro, posicaoJogador1, "jogador 1");}
+
+    public static void posicaoValidaJogador2(char posicaoJogador2){
+        while((posicaoJogador2 == '1' && (tabuleiro[0][0] == 'X' || tabuleiro[0][0] == 'O'))
+                || (posicaoJogador2 == '2' && (tabuleiro[0][2] == 'X' || tabuleiro[0][2] == 'O'))
+                || (posicaoJogador2 == '3' && (tabuleiro[0][4] == 'X' || tabuleiro[0][4] == 'O'))
+                || (posicaoJogador2 == '4' && (tabuleiro[1][0] == 'X' || tabuleiro[1][0] == 'O'))
+                || (posicaoJogador2 == '5' && (tabuleiro[1][2] == 'X' || tabuleiro[1][2] == 'O'))
+                || (posicaoJogador2 == '6' && (tabuleiro[1][4] == 'X' || tabuleiro[1][4] == 'O'))
+                || (posicaoJogador2 == '7' && (tabuleiro[2][0] == 'X' || tabuleiro[2][0] == 'O'))
+                || (posicaoJogador2 == '8' && (tabuleiro[2][2] == 'X' || tabuleiro[2][2] == 'O'))
+                || (posicaoJogador2 == '9' && (tabuleiro[2][4] == 'X' || tabuleiro[2][4] == 'O'))){
+            System.out.println("Jogada inválda, escolha outra posição: ");
+            Scanner jogada = new Scanner(System.in);
+            posicaoJogador2 = jogada.next().charAt(0);
+
+        } marcaPosicao(tabuleiro, posicaoJogador2, "jogador 2");}
+
+    public static void verificarGanhador(char[][] tabuleiro, String jogadorChar, String nomeJogador) {
+
+        String linha1 = String.valueOf((tabuleiro[0][0])) + String.valueOf((tabuleiro[0][2])) + String.valueOf((tabuleiro[0][4]));
+        String linha2 = String.valueOf((tabuleiro[1][0])) + String.valueOf((tabuleiro[1][2])) + String.valueOf((tabuleiro[1][4]));
+        String linha3 = String.valueOf((tabuleiro[2][0])) + String.valueOf((tabuleiro[2][2])) + String.valueOf((tabuleiro[2][4]));
+
+        String coluna1 = String.valueOf((tabuleiro[0][0])) + String.valueOf((tabuleiro[1][0])) + String.valueOf((tabuleiro[2][0]));
+        String coluna2 = String.valueOf((tabuleiro[0][2])) + String.valueOf((tabuleiro[1][2])) + String.valueOf((tabuleiro[2][2]));
+        String coluna3 = String.valueOf((tabuleiro[0][4])) + String.valueOf((tabuleiro[1][4])) + String.valueOf((tabuleiro[2][4]));
+
+        String diagonal1 = String.valueOf((tabuleiro[0][0])) + String.valueOf((tabuleiro[1][2])) + String.valueOf((tabuleiro[2][4]));
+        String diagonal2 = String.valueOf((tabuleiro[0][4])) + String.valueOf((tabuleiro[1][2])) + String.valueOf((tabuleiro[2][0]));
+
+
+        if (linha1.equals(jogadorChar) ||
+                linha2.equals(jogadorChar) ||
+                linha3.equals(jogadorChar) ||
+                coluna1.equals(jogadorChar) ||
+                coluna2.equals(jogadorChar) ||
+                coluna3.equals(jogadorChar) ||
+                diagonal1.equals(jogadorChar) ||
+                diagonal2.equals(jogadorChar)) {
+            Winner = true;
+
+            System.out.println(nomeJogador + ", você venceu!");
+
+
+        } else if (turns == 9) {
+            Draw = true;
+            System.out.println("VELHA! Deseja jogar outra partida?");
+            System.out.println("| 1 - SIM   | 2 - NÃO  |");
+            switch (input.nextInt()) {
+                case 1:
+                    resetaTabuleiro();
+                    playGame();
+                    break;
+                case 2:
+                    System.out.println("Ah, que pena! Até a próxima!");
+                    input.close();
+                    break;
+            }
+        }
+
+
+    }
+
+    public static void getPlacar(String nomeJogadorVencedor, int pontosVencedor, String nomeOutroJogador, int pontosOutroJogador) {
+        System.out.println("Placar:");
+        System.out.println(nomeJogadorVencedor + ": " + pontosVencedor + " X " + nomeOutroJogador + ": " + pontosOutroJogador);
+        System.out.println("Revanche?");
+        System.out.println("| 1 - SIM   | 2 - NÃO  |");
+        switch (input.nextInt()) {
+            case 1:
+
+                resetaTabuleiro();
+                playGame();
+                break;
+            case 2:
+                System.out.println("Ah, que pena! Até a próxima!");
+                input.close();
+                break;
+
+        }
+    }
+
+    public static void resetaTabuleiro() {
+        turns = 0;
+        Winner = false;
+        Draw = false;
+        tabuleiro = new char[][]{
+                {'1', '|', '2', '|', '3'},
+                {'4', '|', '5', '|', '6'},
+                {'7', '|', '8', '|', '9'}};
     }
 }
